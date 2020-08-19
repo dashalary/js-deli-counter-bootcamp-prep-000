6c2208251203506e5e18397eec9aa151e4fbff2d
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-19 19:54:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ katzDeliLine.push(name)
      peopleInLine.push(`${i + 1}. ${katzDeliLine[i]}`);
      
    }
-   return `The current line is ${peopleInLine.join(", ")}`;
+   return `The line is currently: ${peopleInLine.join(", ")}`;
  }
  }
